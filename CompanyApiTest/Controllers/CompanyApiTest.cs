a7dd078a78cd92ba131f60ff623da5dc23ccfcd4
shiwei.fu <shiwei.fu@thoughtworks.com>
2022-10-17 11:07:41 +0800

    refactor: refactor test extract Method build http client

--- CompanyApiTest/Controllers/CompanyApiTest.cs
+++ CompanyApiTest/Controllers/CompanyApiTest.cs
@@ -20,11 +20,9 @@ namespace CompanyApiTest.Controllers
         public async Task Should_can_add_company_when_call_post_api_given_company_not_add()
         {
             //given
-            var testServer = new TestServer(new WebHostBuilder().UseStartup<Startup>());
-            var httpClient = testServer.CreateClient();
+            var httpClient = GetHttpClient();
             var company = new Company("testCompany");
-            var serializeObject = JsonConvert.SerializeObject(company);
-            var content = new StringContent(serializeObject, Encoding.UTF8, "application/json");
+            var content = SerializeCompanyToJson(company);
             //when
             var httpResponseMessage = await httpClient.PostAsync(url, content);
             //then
@@ -34,6 +32,20 @@ namespace CompanyApiTest.Controllers
             Assert.NotNull(companyAdded.Id);
             Assert.Equal(company.Name, companyAdded.Name);
             Assert.Equal(HttpStatusCode.Created, httpResponseMessage.StatusCode);
+        }
+
+        private static StringContent SerializeCompanyToJson(Company company)
+        {
+            var serializeObject = JsonConvert.SerializeObject(company);
+            var content = new StringContent(serializeObject, Encoding.UTF8, "application/json");
+            return content;
+        }
+
+        private static HttpClient GetHttpClient()
+        {
+            var testServer = new TestServer(new WebHostBuilder().UseStartup<Startup>());
+            var httpClient = testServer.CreateClient();
+            return httpClient;
         }
     }
 }
