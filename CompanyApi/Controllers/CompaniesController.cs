0165598c62ee96fbaf558760e3a43d0ce6a9015f
shiwei.fu <shiwei.fu@thoughtworks.com>
2022-10-17 11:14:08 +0800

    feat: check name duplicated when add company

--- CompanyApi/Controllers/CompaniesController.cs
+++ CompanyApi/Controllers/CompaniesController.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CompanyApi.Controllers
@@ -9,9 +10,20 @@ namespace CompanyApi.Controllers
     public class CompaniesController : ControllerBase
     {
         private static List<Company> companies = new List<Company>();
+        public static List<Company> Companies
+        {
+            get => companies;
+            set => companies = value;
+        }
+
         [HttpPost]
         public ActionResult<Company> Add(Company company)
         {
+            if (companies.Any(addedCompany => addedCompany.Name.Equals(company.Name)))
+            {
+                return BadRequest();
+            }
+
             company.Id = Guid.NewGuid().ToString();
             companies.Add(company);
             return new CreatedResult("/companies", company);
--- CompanyApiTest/Controllers/CompanyApiTest.cs
+++ CompanyApiTest/Controllers/CompanyApiTest.cs
@@ -1,8 +1,10 @@
+using System.Collections.Generic;
 using System.Net;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using CompanyApi;
+using CompanyApi.Controllers;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.AspNetCore.TestHost;
@@ -20,6 +22,7 @@ namespace CompanyApiTest.Controllers
         public async Task Should_can_add_company_when_call_post_api_given_company_not_add()
         {
             //given
+            CompaniesController.Companies = new List<Company>();
             var httpClient = GetHttpClient();
             var company = new Company("testCompany");
             var content = SerializeCompanyToJson(company);
@@ -32,6 +35,21 @@ namespace CompanyApiTest.Controllers
             Assert.NotNull(companyAdded.Id);
             Assert.Equal(company.Name, companyAdded.Name);
             Assert.Equal(HttpStatusCode.Created, httpResponseMessage.StatusCode);
+        }
+
+        [Fact]
+        public async Task Should_return_400_when_name_duplicated()
+        {
+            //given
+            CompaniesController.Companies = new List<Company>();
+            var httpClient = GetHttpClient();
+            var company = new Company("testCompany");
+            var content = SerializeCompanyToJson(company);
+            await httpClient.PostAsync(url, content);
+            //when
+            var rspMessage = await httpClient.PostAsync(url, content);
+            //then
+            Assert.Equal(HttpStatusCode.BadRequest, rspMessage.StatusCode);
         }
 
         private static StringContent SerializeCompanyToJson(Company company)
